d1eedc486da371a4bd746cb53463a03fd0a504fd
Yuichi Fujiki <yuichi@chaione.com>
2018-12-10 14:50:43 -0700

    Added necessary assets to the project.

--- app/src/main/java/com/yfujiki/androidpeekaboo/MainActivity.kt
+++ app/src/main/java/com/yfujiki/androidpeekaboo/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.yfujiki.androidpeekaboo
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
