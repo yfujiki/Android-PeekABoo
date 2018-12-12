56731250dac1805d24501fefb43c52eda6426452
Yuichi Fujiki <yuichi@chaione.com>
2018-12-11 17:42:25 -0700

    Use SnapHelper to snap to a single page. Maybe I should use ViewPager though.

--- app/src/main/java/com/yfujiki/androidpeekaboo/MainActivity.kt
+++ app/src/main/java/com/yfujiki/androidpeekaboo/MainActivity.kt
@@ -5,16 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.ViewTreeObserver
 import androidx.paging.PagedList
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.Executors
-import androidx.annotation.NonNull
 import java.util.concurrent.Executor
-
+import androidx.recyclerview.widget.LinearSnapHelper
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentPosition: Int = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +37,33 @@ class MainActivity : AppCompatActivity() {
         val adapter = ImagePagedListAdapter()
         adapter.submitList(list)
         recyclerView.adapter = adapter
+
+        val snapHelper = object : LinearSnapHelper() {
+            override fun findTargetSnapPosition(
+                layoutManager: RecyclerView.LayoutManager?,
+                velocityX: Int,
+                velocityY: Int
+            ): Int {
+                val centerView = findSnapView(layoutManager!!) ?: return RecyclerView.NO_POSITION
+                var targetPosition = layoutManager.getPosition(centerView)
+
+                if (velocityX > 0) {
+                    targetPosition += 1
+                } else if (velocityX < 0) {
+                    targetPosition -= 1
+                }
+
+                if (targetPosition > currentPosition) {
+                    targetPosition = currentPosition + 1
+                } else if (targetPosition < currentPosition) {
+                    targetPosition = currentPosition - 1
+                }
+
+                currentPosition = targetPosition
+                return targetPosition
+            }
+        }
+        snapHelper.attachToRecyclerView(recyclerView)
     }
 }
 
